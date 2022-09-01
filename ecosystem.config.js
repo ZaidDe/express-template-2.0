0926c2b173b3e6b650b1f58d651036b2b58b20f5
Syed Mohani <syed.mohani@ibm.com>
2022-09-02 04:56:08 +0500

    add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: "your-app-name-here",
+        script: "./bin/www",
+        env_production: {
+            NODE_ENV: "production",
+            PORT: 3000
+        },
+        env_development: {
+            NODE_ENV: "development",
+            PORT: 3001
+        }
+    }]
+}
